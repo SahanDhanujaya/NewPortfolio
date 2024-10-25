c98afd66f6ab8426c3ecb4be3d98b19d00ae8543
SahanDhanujaya <sahandhanujaya18@gmail.com>
2024-10-25 15:02:22 +0530

    many sections are done

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ document.getElementById("scrollbutton").addEventListener("click",()=>{
     document.getElementById("welcome").style.display="none";
     
 });
+//  let ar = ["assets/imgs/401069105_860626685533720_1764739307085247302_n.jpg","assets/imgs/432277000_925106579085730_5372217276935642645_n.jpg"];
+//  setInterval(function (){
+//     let last_img = ar.pop();
+//     ar.unshift(last_img);
+//     document.querySelector("#education>section>img").src = ar[0];
+//  } ,3000);
